1873d10d6d984247cc4e829941f8cef9731761a4
SMOKE5 <metallica5875@yahoo.com>
2016-08-30 12:37:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PSID_Dumper
+
+Tool to dump a users PSID as well as an example for testing users PSID to ensure its running on the console you want it to
+
+I hope homebrew developers will use this system when using testers on sensative projects. This will help prevent leaks
---
+++ example/main.c
@@ -0,0 +1,41 @@
+#include <psp2/display.h>
+#include <psp2/kernel/processmgr.h>
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "graphics.h"
+
+#define printf psvDebugScreenPrintf
+
+typedef struct SceKernelOpenPsId {
+	char id[16];
+} SceKernelOpenPsId;
+
+int sceKernelGetOpenPsId(SceKernelOpenPsId *id);
+
+int main() {
+	psvDebugScreenInit();
+
+	// Use users PSID here
+	SceKernelOpenPsId my_psid = {{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}};
+	SceKernelOpenPsId id;
+	sceKernelGetOpenPsId(&id);
+
+	if (memcmp(&id, &my_psid, 0x10) == 0){
+		printf("Success! PSID matches!\n");
+		printf("\nAuto-exiting in 5 seconds...");
+		sceKernelDelayThread(5 * 1000 * 1000);
+		sceKernelExitProcess(0);
+	}
+	else {
+		printf("PSID does NOT match!\n");
+		printf("\nAuto-exiting in 5 seconds...");
+		sceKernelDelayThread(5 * 1000 * 1000);
+		sceKernelExitProcess(0);
+	}
+
+	return 0;
+}
